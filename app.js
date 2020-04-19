0fd6eb626cac75a3818cad12b2707950456ed6d6
Miguel Angel <mgangsal@outlook.com>
2020-04-19 12:03:46 -0600

    Browserify added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const indexRoute = require('./routes/index');
+const browserify = require('browserify-middleware');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'pug');
+
+// Create a browser version of javascript files.
+app.use('/js', browserify(path.join(__dirname, 'public', 'js')));
 
 app.use(express.static('public'))
 
