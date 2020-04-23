bafe491ea007ba2c846ffe96c30260afe09a453d
Miguel Angel <mgangsal@outlook.com>
2020-04-23 13:02:47 -0500

    Initial setup for creating users.

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const browserify = require('browserify-middleware');
 const path = require('path');
+const app = express();
 
 const paths = require('./controller/paths');
-
-const app = express();
+const secrets = require('./util/secrets');
 
 const shopRoutes = require('./routes/shop');
 
+const session = require('express-session');
+
 app.set('view engine', 'pug');
+
+app.use(session({
+  secret: secrets.secret,
+  secure: false,
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use(paths);
 
+// Convert to JS-Browser
 app.use('/js', browserify(path.join(__dirname, 'public', 'js')));
 
+// Create a static folder for using in the views.
 app.use(express.static('public'))
 
+// Create the basic routes.
 app.use(shopRoutes);
 
+// Start the server.
 app.listen(3000);
---
+++ util/secrets.js
@@ -0,0 +1,3 @@
+const mongoURI = "mongodb+srv://root:guapon12@cluster0-eggrn.mongodb.net/mdireynosa";
+module.exports.secret = "JustM0N1K4 MY FR1END";
+module.exports.mongoURI = mongoURI;
